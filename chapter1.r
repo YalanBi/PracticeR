f6e616927c97e37d56b72dec1019e665c8d7d851
YalanBi <biyalan@yeah.net>
2012-11-05 11:48:51 +0100

    until 1.3

--- chapter1.r
+++ chapter1.r
@@ -1,2 +1,22 @@
 #Assigment 1.1
-for(x in 1:10) cat(x,"A\n")
+??sum
+?lm
+#Assigment 1.2
+1:10 #One to ten
+seq(1,10) #One to ten
+seq(1,50,4) #One to fifty, with step of 4
+cat("ind",1:10,sep=",")
+cat("ind",seq(1,10,2),sep=",")
+paste("ind",1:10,sep=" ")
+sqrt((1+5)/4)
+12/2==6 #Comparison of numbers
+12/2==3
+1:10<7 #Comparison of vector and a number
+seq(1,10,2)<7
+1:10/7 #Add/Subtrract/Multiply/Divide a vector
+1:10*1:10 #Add/Subtrract/Multiply/Divide a vector with a vector
+1:10*seq(1,10,3)
+1:10*seq(1,10,7)
+matrix(2,3,5)
+matrix(2,3,5)*3 #Matrices
+#Assigment 1.3
