b9c64255e67365a61b41938d601b80da28741ef4
YalanBi <biyalan@yeah.net>
2012-11-06 17:07:54 +0100

    fuctions!

--- chapter3.r
+++ chapter3.r
@@ -12,3 +12,36 @@ divide <- function(number){
 }
 divide(100.3) #calculation
 invisible(divide(100.3)) #prevent the result printed
+#Q3.1
+x <- 1:10
+y <- 1:10
+hypotenuse <- fuction(x,y){
+	res <- sqrt(x*x + y*y) #hypotenuse of a right-angled triangle
+	res
+}
+#Q3.2
+x <- 1
+y <- 1
+while(x<=5){
+	y <- y*x
+	x <- x+1
+}
+y
+
+faculty <- function(x){
+	n <- 2
+	result <- 1
+	while(n <= x){
+		result <- result*n
+		n <- n + 1
+	}
+	return(result)
+}
+
+faculty <- function(x){
+	if(x==1) return(1)
+	return(x*faculty(x-1))
+}
+
+
+#Q3.3
